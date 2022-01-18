d25027863e316316b5f4f1c84dbff1baebee3881
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2022-01-18 12:27:45 -0300

    Create codeExercise-randomisationPasswordGenerator.swift

---
+++ codeExercise-randomisationPasswordGenerator.swift
@@ -0,0 +1,32 @@
+/*
+  You are going to create a password generator. we have created an array called alphabet which contains all 26 letters in the alphabet.
+  
+  Write some code to randomly pick 6 letters out of the alphabet to create a random 6 character password.
+  
+  You can test out your code here: https://repl.it/@appbrewery/randomisation-exercise#main.swift
+  
+  NOTE: The variable password should be a String of characters like "vacbae" or "abcefg"
+*/
+
+// resolution:
+
+func exercise() {   
+
+    let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    
+    let password = generatePassword(alphabet)
+    
+    print(password)
+}
+
+func generatePassword(_ alphabet: [String]) -> String {
+    var password: String = ""
+    
+    for _ in 1...6 {
+        password += alphabet.randomElement() ?? ""
+    }
+    
+    return password
+}
+
+exercise()
